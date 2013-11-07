4f5d0beb0f4a625c3b109b485d026caf916814a8
Ben Johnson <circuitben@gmail.com>
2013-11-07 12:29:39 -0800

    Ported to SDK 2.0

--- src/sddt.c
+++ src/sddt.c
@@ -1,52 +1,43 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
-#define MY_UUID { 0xA2, 0x10, 0x8A, 0xDB, 0x8A, 0x31, 0x41, 0xF5, 0xAA, 0x5E, 0x72, 0x46, 0xF4, 0xCE, 0xE9, 0x3D }
-PBL_APP_INFO(MY_UUID,
-             "SDDT", "circuitben.net",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_WATCH_FACE);
-
-Window window;
+Window *window;
 
 // Hopefully these buffers are big enough for any localization.
 // They're big enough for English at least.
 
 char date_text[32];
-TextLayer date_layer;
+TextLayer *date_layer;
 
 char hour_minutes_text[] = "00:00";
-TextLayer hour_minutes_layer;
+TextLayer *hour_minutes_layer;
 
 char seconds_text[] = ":00";
-TextLayer seconds_layer;
+TextLayer *seconds_layer;
 
 char ampm_text[] = "AM";
-TextLayer ampm_layer;
+TextLayer *ampm_layer;
 
 // This was the time at the last call to update_time().
 // This is initialized with impossible values to ensure all layers are updated
 // on the first call to update_time().
-PblTm last_time = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+struct tm last_time = {-1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0};
 
 // State of clock_is_24h_style() at the last call to update_time().
 // We don't care about the initial value of this because the difference
 // in last_time will cause everything to be updated.
 bool last_24hr;
 
-Layer decoration_layer;
+Layer *decoration_layer;
 
-void update_time(const PblTm *time)
+void update_time(struct tm *time, TimeUnits units_changed)
 {
     // Update text layers for time fields that have changed.
 
     // Month and day.
     if (time->tm_mon != last_time.tm_mon || time->tm_mday != last_time.tm_mday)
     {
-        string_format_time(date_text, sizeof(date_text), "%A\n%B %e", time);
-        text_layer_set_text(&date_layer, date_text);
+        strftime(date_text, sizeof(date_text), "%A\n%B %e", time);
+        text_layer_set_text(date_layer, date_text);
     }
 
     bool is_24hr = clock_is_24h_style();
@@ -54,7 +45,7 @@ void update_time(const PblTm *time)
     // Hour and minute.
     if (time->tm_hour != last_time.tm_hour || time->tm_sec != last_time.tm_sec || is_24hr != last_24hr)
     {
-        // Use snprintf instead of string_format_time() to get the right padding.
+        // Use snprintf instead of strftime() to get the right padding.
         const char *time_format;
         int hour = time->tm_hour;
         if (is_24hr)
@@ -76,7 +67,7 @@ void update_time(const PblTm *time)
             }
         }
         snprintf(hour_minutes_text, sizeof(hour_minutes_text), time_format, hour, time->tm_min);
-        text_layer_set_text(&hour_minutes_layer, hour_minutes_text);
+        text_layer_set_text(hour_minutes_layer, hour_minutes_text);
     }
 
     // AM/PM (blank for 24-hour style).
@@ -84,18 +75,18 @@ void update_time(const PblTm *time)
     {
         if (is_24hr)
         {
-            text_layer_set_text(&ampm_layer, "");
+            text_layer_set_text(ampm_layer, "");
         } else {
-            string_format_time(ampm_text, sizeof(ampm_text), "%p", time);
-            text_layer_set_text(&ampm_layer, ampm_text);
+            strftime(ampm_text, sizeof(ampm_text), "%p", time);
+            text_layer_set_text(ampm_layer, ampm_text);
         }
     }
 
     // Seconds.
     if (time->tm_sec != last_time.tm_sec)
     {
-        string_format_time(seconds_text, sizeof(seconds_text), ":%S", time);
-        text_layer_set_text(&seconds_layer, seconds_text);
+        strftime(seconds_text, sizeof(seconds_text), ":%S", time);
+        text_layer_set_text(seconds_layer, seconds_text);
     }
 
     // Save state for next time.
@@ -103,14 +94,15 @@ void update_time(const PblTm *time)
     last_24hr = is_24hr;
 }
 
-void add_text_layer(TextLayer *layer, GRect rect, GFont font, GTextAlignment align)
+TextLayer *add_text_layer(GRect rect, GFont font, GTextAlignment align)
 {
-    text_layer_init(layer, rect);
+    TextLayer *layer = text_layer_create(rect);
     text_layer_set_text_color(layer, GColorWhite);
     text_layer_set_background_color(layer, GColorClear);
     text_layer_set_font(layer, font);
     text_layer_set_text_alignment(layer, align);
-    layer_add_child(&window.layer, &layer->layer);
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(layer));
+    return layer;
 }
 
 void update_decoration(Layer *layer, GContext *ctx)
@@ -121,46 +113,32 @@ void update_decoration(Layer *layer, GContext *ctx)
     graphics_draw_line(ctx, GPoint(0, 69), GPoint(144, 69));
 }
 
-void handle_init(AppContextRef ctx)
+int main()
 {
-    window_init(&window, "SDDT");
-    window_stack_push(&window, true);
-    window_set_background_color(&window, GColorBlack);
+    window = window_create();
+    window_stack_push(window, true);
+    window_set_background_color(window, GColorBlack);
 
     GFont font_condensed = fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21);
     GFont font_bold = fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49);
 
-    add_text_layer(&date_layer,         GRect(8, 8, 128, 52),   font_condensed, GTextAlignmentCenter);
-    add_text_layer(&seconds_layer,      GRect(104, 80, 32, 22), font_condensed, GTextAlignmentRight);
-    add_text_layer(&ampm_layer,         GRect(8, 80, 32, 22),   font_condensed, GTextAlignmentLeft);
-    add_text_layer(&hour_minutes_layer, GRect(8, 96, 128, 50),  font_bold,      GTextAlignmentCenter);
+    date_layer = add_text_layer(GRect(8, 8, 128, 52), font_condensed, GTextAlignmentCenter);
+    seconds_layer = add_text_layer(GRect(104, 80, 32, 22), font_condensed, GTextAlignmentRight);
+    ampm_layer = add_text_layer(GRect(8, 80, 32, 22), font_condensed, GTextAlignmentLeft);
+    hour_minutes_layer = add_text_layer(GRect(8, 96, 128, 50), font_bold, GTextAlignmentCenter);
 
-    layer_init(&decoration_layer, window.layer.frame);
-    decoration_layer.update_proc = &update_decoration;
-    layer_add_child(&window.layer, &decoration_layer);
+    decoration_layer = layer_create(GRect(0, 0, 144, 168));
+    layer_set_update_proc(decoration_layer, update_decoration);
+    layer_add_child(window_get_root_layer(window), decoration_layer);
 
     // Update the time now.
-    PblTm time;
-    get_time(&time);
-    update_time(&time);
-}
+    time_t t = time(0);
+    struct tm *time = localtime(&t);
+    update_time(time, -1);
 
-void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t)
-{
-    update_time(t->tick_time);
-}
+    tick_timer_service_subscribe(SECOND_UNIT, update_time);
 
-void pbl_main(void *params)
-{
-    PebbleAppHandlers handlers =
-    {
-        .init_handler = &handle_init,
+    app_event_loop();
 
-        .tick_info =
-        {
-          .tick_handler = &handle_second_tick,
-          .tick_units = SECOND_UNIT
-        }
-    };
-    app_event_loop(params, &handlers);
+    return 0;
 }
